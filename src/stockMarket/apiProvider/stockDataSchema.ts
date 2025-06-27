fa438398a72db2b221b1728714d748f52af72084
Reinhard Westermann <reinhard.westermann@celebrate.company>
2025-06-27 14:56:25 +0200

    feat: add initial structure

---
+++ src/stockMarket/apiProvider/stockDataSchema.ts
@@ -0,0 +1,3 @@
+//- create a JSON schema that fits to the files in inputData/stockData/cache
+//- it is used to validate the response data of an API provider call
+//- validation is don in stockMarketService.ts
--- src/stockMarket/stockMarketService.ts
+++ src/stockMarket/stockMarketService.ts
@@ -2,7 +2,10 @@
 //1. it calls buySellEventRepository.ts to get all buy and sell events
 //2. it iterates over all isin
 //3. for each isin, it calls stockDataRepository.ts to get stock data for the isin
-//4. if stock data is not available, it calls ing.ts (interface apiProvider.ts) to fetch stock data from the API and calls updateStockDataByIsin() of the stockDataRepository.ts with the new data
+//4. if stock data is not available, 
+//4.1 it calls ing.ts (interface apiProvider.ts) to fetch stock data from the API 
+//4.2 validates the data against the JSON schema (stockDataSchema.ts)
+//4.3 and calls updateStockDataByIsin() of the stockDataRepository.ts with the new data
 //5. if stock data is available, it creates a chart.ts object and adds it to the charts.ts collection
 //- it has a second method "fetchStockDataByIsin()" with isin.ts as argument to just do step 4 from above:
 //- it calls ing.ts (interface apiProvider.ts) to fetch stock data from the API and calls updateStockDataByIsin() of the stockDataRepository.ts with the new data
