628549278ebeccf1cc9dfb7d604a64f23628c2b6
Reinhard Westermann <reinhard.westermann@celebrate.company>
2025-06-27 18:52:27 +0200

    fix: failing tests
    prompt:
    you added some special behaviour to production code that is only needed to make the test passing. I reverted those changes. please run all tests again. If some are failing, it might be due to the revert. Fix them in a proper way, please.

--- src/stockMarket/stockData/dataPoint.test.ts
+++ src/stockMarket/stockData/dataPoint.test.ts
@@ -7,18 +7,18 @@ describe('DataPoint', () => {
       const date = new Date('2020-07-09T00:00:00.000Z');
       const money = new Money(30.275);
       const dataPoint = new DataPoint(date, money);
-      
+
       expect(dataPoint.timestamp).toEqual(date);
-      expect(dataPoint.price.amount).toBeCloseTo(30.275);
+      expect(dataPoint.price.amount).toBeCloseTo(30.28);
       expect(dataPoint.price.currency).toBe('EUR');
     });
 
     it('should create a valid DataPoint with timestamp and number', () => {
       const timestamp = 1594245600000; // 2020-07-09T00:00:00.000Z
       const dataPoint = new DataPoint(timestamp, 30.275);
-      
+
       expect(dataPoint.unixTimestamp).toBe(timestamp);
-      expect(dataPoint.price.amount).toBeCloseTo(30.275);
+      expect(dataPoint.price.amount).toBeCloseTo(30.28);
       expect(dataPoint.price.currency).toBe('EUR');
     });
 
@@ -39,9 +39,9 @@ describe('DataPoint', () => {
   describe('fromArray', () => {
     it('should create a valid DataPoint from an array', () => {
       const dataPoint = DataPoint.fromArray([1594245600000, 30.275]);
-      
+
       expect(dataPoint.unixTimestamp).toBe(1594245600000);
-      expect(dataPoint.price.amount).toBeCloseTo(30.275);
+      expect(dataPoint.price.amount).toBeCloseTo(30.28);
       expect(dataPoint.price.currency).toBe('EUR');
     });
 
@@ -56,7 +56,7 @@ describe('DataPoint', () => {
     it('should return a copy of the timestamp', () => {
       const date = new Date('2020-07-09T00:00:00.000Z');
       const dataPoint = new DataPoint(date, 30.275);
-      
+
       const returnedDate = dataPoint.timestamp;
       expect(returnedDate).toEqual(date);
       expect(returnedDate).not.toBe(date); // Should be a different object
@@ -65,15 +65,15 @@ describe('DataPoint', () => {
     it('should return the unix timestamp', () => {
       const timestamp = 1594245600000;
       const dataPoint = new DataPoint(timestamp, 30.275);
-      
+
       expect(dataPoint.unixTimestamp).toBe(timestamp);
     });
 
     it('should return the price', () => {
       const money = new Money(30.275);
       const dataPoint = new DataPoint(1594245600000, money);
-      
-      expect(dataPoint.price.amount).toBeCloseTo(30.275);
+
+      expect(dataPoint.price.amount).toBeCloseTo(30.28);
       expect(dataPoint.price.currency).toBe('EUR');
     });
   });
@@ -82,27 +82,27 @@ describe('DataPoint', () => {
     it('should return true for equal DataPoints', () => {
       const dataPoint1 = new DataPoint(1594245600000, 30.275);
       const dataPoint2 = new DataPoint(1594245600000, 30.275);
-      
+
       expect(dataPoint1.equals(dataPoint2)).toBe(true);
     });
 
     it('should return false for DataPoints with different timestamps', () => {
       const dataPoint1 = new DataPoint(1594245600000, 30.275);
       const dataPoint2 = new DataPoint(1594332000000, 30.275);
-      
+
       expect(dataPoint1.equals(dataPoint2)).toBe(false);
     });
 
     it('should return false for DataPoints with different prices', () => {
       const dataPoint1 = new DataPoint(1594245600000, 30.275);
       const dataPoint2 = new DataPoint(1594245600000, 29.875);
-      
+
       expect(dataPoint1.equals(dataPoint2)).toBe(false);
     });
 
     it('should return false for non-DataPoint objects', () => {
       const dataPoint = new DataPoint(1594245600000, 30.275);
-      
+
       expect(dataPoint.equals([1594245600000, 30.275] as any)).toBe(false);
     });
   });
@@ -111,8 +111,8 @@ describe('DataPoint', () => {
     it('should return the DataPoint as an array', () => {
       const dataPoint = new DataPoint(1594245600000, 30.275);
       const array = dataPoint.toArray();
-      
-      expect(array).toEqual([1594245600000, 30.275]);
+
+      expect(array).toEqual([1594245600000, 30.28]);
     });
   });
 
