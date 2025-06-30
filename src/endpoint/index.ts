180666b9dfe75888ed6fe7b2848af23df7ac9730
Reinhard Westermann <reinhard.westermann@celebrate.company>
2025-06-30 17:27:17 +0200

    chore: show stock name in chart header

--- src/endpoint/index.ts
+++ src/endpoint/index.ts
@@ -57,7 +57,7 @@ app.get('/', async (req, res) => {
       html += `
         <div class="chart-container" id="chart-container-${isin}">
           <div class="chart-header">
-            <h2>${isin}</h2>
+            <h2>${isin} ${chart.isinEvents.events[0].stockName}</h2>
             <button class="update-button" data-isin="${isin}">Update</button>
           </div>
           <div class="error-message" id="error-${isin}"></div>
